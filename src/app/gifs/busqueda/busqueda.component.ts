0af5e2c7b35419a077b2d8ea52e3bc7bb1c3e89d
irvinzato <multizato@hotmail.com>
2022-05-30 21:31:48 -0500

    Uso de referencia local en HTML y ViewChield con todas sus especificaciones, parecido a querySelector

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-busqueda',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BusquedaComponent implements OnInit {
 
+  @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  buscar() {
+    const valor = this.txtBuscar.nativeElement.value;
+    console.log(valor);
+    this.txtBuscar.nativeElement.value = '';
   }
 
 }
